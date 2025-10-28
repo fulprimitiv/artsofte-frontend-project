8e1db4aacf3c63662b5c000ee6e0a5ca3eebf91f
fulprimitiv <eremeevdima51@gmail.com>
2025-10-28 09:29:28 +0500

    feat: страница со списком компаний

--- src/app/components/company-item/company-item.ts
+++ src/app/components/company-item/company-item.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Company } from '../../data/interfaces/company-interface';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-company-item',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './company-item.html',
   styleUrl: './company-item.scss',
 })
 export class CompanyItemComponent {
-
+  @Input() company!: Company;
 }
--- src/app/pages/company-list/company-list.ts
+++ src/app/pages/company-list/company-list.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CompanyService } from '../../data/services/company-service';
+import { CompanyItemComponent } from '../../components/company-item/company-item';
+import { Observable, map } from 'rxjs';
+import { Company } from '../../data/interfaces/company-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-company-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CompanyItemComponent],
   templateUrl: './company-list.html',
-  styleUrl: './company-list.scss',
+  styleUrls: ['./company-list.scss'],
 })
 export class CompanyListComponent {
+  private readonly companyService = inject(CompanyService);
 
+  companies$: Observable<Company[]> = this.companyService
+    .getCompanies()
+    .pipe(map(res => res.data));
+
+  trackById(index: number, company: Company): number {
+    return company.id;
+  }
 }
