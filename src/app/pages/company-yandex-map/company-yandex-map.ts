231d538ed3dde760a2a9928ef02e05311e7ba194
fulprimitiv <eremeevdima51@gmail.com>
2025-10-28 07:42:52 +0500

    feat: заглушка для пути /map

--- src/app/pages/company-yandex-map/company-yandex-map.ts
+++ src/app/pages/company-yandex-map/company-yandex-map.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-company-yandex-map',
-  imports: [],
   templateUrl: './company-yandex-map.html',
   styleUrl: './company-yandex-map.scss',
 })
 export class CompanyYandexMapComponent {
+  @Input() lat!: number;
+  @Input() lon!: number;
 
+  constructor(private sanitizer: DomSanitizer) { }
+
+  get mapUrl(): SafeResourceUrl {
+    const url = `https://yandex.ru/map-widget/v1/?ll=${this.lon},${this.lat}&z=15&pt=${this.lon},${this.lat},pm2rdm`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
