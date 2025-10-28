9fb144893a59abb9d3a2c3edb8e410babdc12491
fulprimitiv <eremeevdima51@gmail.com>
2025-10-28 10:15:51 +0500

    feat: страница с подробной информации о компании

--- src/app/pages/company-detail/company-detail.ts
+++ src/app/pages/company-detail/company-detail.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CompanyService } from '../../data/services/company-service';
+import { Company } from '../../data/interfaces/company-interface';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-company-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './company-detail.html',
   styleUrl: './company-detail.scss',
 })
 export class CompanyDetailComponent {
+  private route = inject(ActivatedRoute);
+  private companyService = inject(CompanyService);
 
+  company$: Observable<Company> = this.route.params.pipe(
+    switchMap(params => this.companyService.getCompanyById(+params['id']))
+  );
 }
