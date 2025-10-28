ad67b29c6860476dc5fb881bfa0d262714fb3ff2
fulprimitiv <eremeevdima51@gmail.com>
2025-10-28 12:00:16 +0500

    feat: сортировка компаний

--- src/app/components/company-sort/company-sort.ts
+++ src/app/components/company-sort/company-sort.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+export type SortField = 'name' | 'type' | 'industry';
 
 @Component({
   selector: 'app-company-sort',
-  imports: [],
   templateUrl: './company-sort.html',
-  styleUrl: './company-sort.scss',
+  styleUrl: './company-sort.scss'
 })
 export class CompanySortComponent {
+  @Output() sortChange = new EventEmitter<SortField>();
 
+  onSort(field: SortField) {
+    this.sortChange.emit(field);
+  }
 }
--- src/app/pages/company-list/company-list.ts
+++ src/app/pages/company-list/company-list.ts
@@ -1,25 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { CompanyService } from '../../data/services/company-service';
-import { CompanyItemComponent } from '../../components/company-item/company-item';
-import { Observable, map } from 'rxjs';
 import { Company } from '../../data/interfaces/company-interface';
-import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
+import { CompanySortComponent, SortField } from '../../components/company-sort/company-sort';
+import { CompanyItemComponent } from '../../components/company-item/company-item';
 
 @Component({
   selector: 'app-company-list',
-  standalone: true,
-  imports: [CommonModule, CompanyItemComponent],
+  imports: [CompanySortComponent, CompanyItemComponent],
   templateUrl: './company-list.html',
-  styleUrls: ['./company-list.scss'],
+  styleUrl: './company-list.scss'
 })
 export class CompanyListComponent {
   private readonly companyService = inject(CompanyService);
 
-  companies$: Observable<Company[]> = this.companyService
-    .getCompanies()
-    .pipe(map(res => res.data));
+  companies: Company[] = [];
+  sortedCompanies: Company[] = [];
 
-  trackById(index: number, company: Company): number {
-    return company.id;
+  constructor() {
+    this.companyService.getCompanies()
+      .pipe(map(res => res.data))
+      .subscribe(data => {
+        this.companies = data;
+        this.sortedCompanies = [...data];
+      });
+  }
+
+  sortCompanies(field: SortField) {
+    this.sortedCompanies = [...this.companies].sort((a, b) => {
+      if (field === 'name') {
+        return a.business_name.localeCompare(b.business_name);
+      }
+      if (field === 'type') {
+        return a.type.localeCompare(b.type);
+      }
+      if (field === 'industry') {
+        return a.industry.localeCompare(b.industry);
+      }
+      return 0;
+    });
   }
 }
